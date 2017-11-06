a8e931377d2f6f7d9b3ef2690e7462d8651f48f9
Zijing Li <w1c9@lulu.ugrad.cs.ubc.ca>
2017-11-06 15:23:52 -0800

    Added CRUD functions for customer and baggage

--- airplanedb.py
+++ airplanedb.py
@@ -178,3 +178,94 @@ class AirplaneDb(object):
         print(('Created new {0} table in {1}').format('SCHEDULE',self.db))
 
         cursor.close()
+
+		#==============================================================================
+#   function: update_customer
+#   description: update fields in customer
+#==============================================================================
+    def update_customer(self, custID, field, new_value):
+        db = MySQLdb.connect(host=self.host, 
+                             user=self.user, 
+                             passwd=self.pw, 
+                             db=self.db)
+        update_customer_query = """ UPDATE CUSTOMER
+                  SET %s = '%s'
+                  WHERE C_ID = %s """ % (field, new_value, custID)
+                                   
+        cursor = self.airdb.cursor()
+        try:
+            cursor.execute(update_customer_query)
+            db.commit()
+            print("Update Customer Success")
+        except:
+            print("Update Customer Failed")
+            db.rollback()
+        
+        db.close()
+
+#==============================================================================
+#   function: add_baggage
+#   description: adds an instance of baggage to BAGGAGE table
+#==============================================================================
+    def add_baggage(self, bag_ID, cust_ID, bag_weight):
+        db = MySQLdb.connect(host=self.host, 
+                             user=self.user, 
+                             passwd=self.pw, 
+                             db=self.db)
+        add_baggage_query = """INSERT INTO BAGGAGE(B_ID,C_ID,B_WEIGHT)
+                                VALUES('%s','%s','%s')""" % (bag_ID, cust_ID, bag_weight)
+        cursor = self.airdb.cursor()
+        
+        try:
+            cursor.execute(add_baggage_query)
+            db.commit()
+            print("Add Baggage Success")
+        except:
+            print("Add Baggage Failed")
+            db.rollback()
+        
+        db.close()
+
+#==============================================================================
+#   function: add_customer
+#   description: adds an instance of customer to CUSTOMER table
+#==============================================================================
+    def add_customer(self, cust_ID, cust_name, cust_age, cust_email, cust_phone):
+         db = MySQLdb.connect(host=self.host, 
+                             user=self.user, 
+                             passwd=self.pw, 
+                             db=self.db)
+         add_customer_query = """INSERT INTO CUSTOMER(C_ID, C_NAME, C_AGE, C_EMAIL, C_PHONE)
+                                 VALUES('%s','%s','%s','%s','%s')""" % (cust_ID, cust_name, cust_age, cust_email, cust_phone)
+         cursor = self.airdb.cursor()
+         try:
+             cursor.execute(add_customer_query)
+             db.commit()
+             print("Add Customer Success")
+         except:
+             print("Add Customer Failed")
+             db.rollback()
+        
+         db.close()
+
+#==============================================================================
+#   function: add_frequent_flier
+#   description: adds a new frequent flier instance to FREQUENTFLIER table
+#==============================================================================    
+    def add_frequent_flier(self, cust_ID, miles):
+        db = MySQLdb.connect(host=self.host, 
+                             user=self.user, 
+                             passwd=self.pw, 
+                             db=self.db)
+        add_ff_query = """ INSERT INTO FREQUENTFLIER(C_ID, FF_MILES)
+                            VALUES('%s', '%s') """ % (cust_ID, miles)
+        cursor = self.airdb.cursor()
+        try:
+            cursor.execute(add_ff_query)
+            db.commit()
+            print("Add Frequent Flier")
+        except:
+            print("Add Customer Failed")
+            db.rollback()
+        
+        db.close()
