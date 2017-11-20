75d609c439f69824717ebaf0a98a2f613cc13c6f
maknoon <conniemob@gmail.com>
2017-11-19 23:38:41 -0800

    User behavior fix

--- airplane.py
+++ airplane.py
@@ -119,7 +119,9 @@ def userspecificUI():
     if request.method == 'POST':
         itinerary_id = request.form['i_id']
         if itinerary_id == '': flash(500)
-        elif airdb.check_itinerary(1, itinerary_id) == 0: flash(500)
+        elif airdb.check_itinerary(1, itinerary_id) == 0:
+            alert_t = 'user_error'
+            flash(200)
         else:
             if 'addbaggage' in request.form:
                 if request.form['b_weight'] == '': flash(500)
